e30996c5a43c42cd419d0a76ad7ea900d50de42f
adithyakam <50355901+adithyakam@users.noreply.github.com>
2021-03-04 07:48:28 +0530

    addding sign in

--- src/SignInScreen.js
+++ src/SignInScreen.js
@@ -4,8 +4,14 @@ import './SignInScreen.css'
 
 function SignInScreen() {
     return (
-        <div>
-            In sgnin
+        <div className='signinscreen'>
+            <form>
+                <h1>Sign In</h1>
+                <input placeholder="Email" type='email'/>
+                <input placeholder="password" type='password'/>
+                <button type='submit'>Sign In</button>
+                <h4><span className='signinscreen_span_text'>New to Netflix?</span><span className='s'> Sign Up now.</span></h4>
+            </form>
         </div>
     )
 }
